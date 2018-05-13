80b47d6a7b815e11c3d5bee6f52006e17c269216
Owen Dwyer <odwyer@gmail.com>
2018-05-13 15:20:05 +0100

    Update 0_main.js

--- js/0_main.js
+++ js/0_main.js
@@ -1,5 +1,6 @@
 
 var opdGame={};
+opdGame.active=false;
 opdGame.Views={};
 opdGame.Modules={};
 
@@ -10,23 +11,27 @@ if(document.readyState==='complete'){
 }
 
 opdGame.init=function(){
-	console.log(opdGame.model.version);
+	//sometimes the 'DOMContentLoaded' event fires twice on the same page
+	if(!opdGame.active){
+		opdGame.active=true;
+		console.log(opdGame.model.version);
 
-	opdWrapper.setup('myCanvas','containerDiv');
-	opdGame.model.orientation=opdWrapper.getOrientation();
-	opdGame.model.canvasRatio=opdWrapper.getCanvasRatio();
-	opdGame.stage=opdWrapper.makeStage();
-	opdGame.view=new opdGame.View();
-	opdGame.stage.addChild(opdGame.view);
-	opdWrapper.setOrientationCallback(opdGame.view.orientationChange);
-	opdWrapper.setResizeCallback(opdGame.view.updateResize);
+		opdWrapper.setup('myCanvas','containerDiv');
+		opdGame.model.orientation=opdWrapper.getOrientation();
+		opdGame.model.canvasRatio=opdWrapper.getCanvasRatio();
+		opdGame.stage=opdWrapper.makeStage();
+		opdGame.view=new opdGame.View();
+		opdGame.stage.addChild(opdGame.view);
+		opdWrapper.setOrientationCallback(opdGame.view.orientationChange);
+		opdWrapper.setResizeCallback(opdGame.view.updateResize);
 
-	if(createjs.BrowserDetect.isIOS)opdGame.model.touchMode=true;
-	if(createjs.BrowserDetect.isAndroid)opdGame.model.touchMode=true;
+		if(createjs.BrowserDetect.isIOS)opdGame.model.touchMode=true;
+		if(createjs.BrowserDetect.isAndroid)opdGame.model.touchMode=true;
 
-	//if this is set to true, then view will only init when 
-	//browser url matches model.siteUrl - see controller.init()
-	opdGame.model.siteLock=false;
+		//if this is set to true, then view will only init when 
+		//browser url matches model.siteUrl - see controller.init()
+		opdGame.model.siteLock=false;
 
-	opdGame.controller.init();
+		opdGame.controller.init();
+	}
 };
